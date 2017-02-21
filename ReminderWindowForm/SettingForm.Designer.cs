de675aa4e86a6b46dbb316cd38703786e97b315b
Vo Nhu Trong <vonhutrong@gmail.com>
2017-02-21 20:58:34 +0700

    simple reminder

---
+++ ReminderWindowForm/SettingForm.Designer.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ReminderWindowForm
+{
+    partial class SettingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SettingForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.message = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timeValue = new System.Windows.Forms.ComboBox();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.settingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Time (min)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Message";
+            // 
+            // message
+            // 
+            this.message.Location = new System.Drawing.Point(93, 49);
+            this.message.Multiline = true;
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(179, 122);
+            this.message.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(105, 192);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timeValue
+            // 
+            this.timeValue.FormattingEnabled = true;
+            this.timeValue.Items.AddRange(new object[] {
+            "15",
+            "30",
+            "45",
+            "60"});
+            this.timeValue.Location = new System.Drawing.Point(93, 12);
+            this.timeValue.Name = "timeValue";
+            this.timeValue.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.timeValue.Size = new System.Drawing.Size(179, 21);
+            this.timeValue.TabIndex = 0;
+            this.timeValue.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.comboBox1_KeyPress);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon.Text = "Reminder";
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.OnClickNotifyIcon);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(112, 48);
+            // 
+            // settingToolStripMenuItem
+            // 
+            this.settingToolStripMenuItem.Name = "settingToolStripMenuItem";
+            this.settingToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.settingToolStripMenuItem.Text = "Setting";
+            this.settingToolStripMenuItem.Click += new System.EventHandler(this.settingToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // SettingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 228);
+            this.Controls.Add(this.timeValue);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.message);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SettingForm";
+            this.Text = "Setting";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SettingForm_FormClosing);
+            this.Load += new System.EventHandler(this.SettingForm_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox message;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ComboBox timeValue;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem settingToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
---
+++ ReminderWindowForm/SettingForm.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReminderWindowForm
+{
+    public partial class SettingForm : Form
+    {
+        private readonly int TIME_UNIT = 60000; //1000 * 60, sum of milliseconds in 1 minute
+
+        private Timer timer = new Timer();
+        private bool allowVisible;
+
+        public SettingForm()
+        {
+            InitializeComponent();
+
+            timeValue.Text = Properties.Settings.Default.timeValue;
+            message.Text = Properties.Settings.Default.message;
+
+            timer.Tick += new EventHandler(OnTimedEvent);
+            notifyIcon.Icon = ReminderWindowForm.Properties.Resources.icon;
+            notifyIcon.Visible = true;
+
+            if (!string.IsNullOrEmpty(Properties.Settings.Default.timeValue))
+            {
+                allowVisible = false;
+            }
+            else
+            {
+                allowVisible = true;
+            }
+
+            //start with windows
+            bool isStartedWithWindows = Properties.Settings.Default.startedWithWindows;
+            if (!isStartedWithWindows)
+            {
+                RegistryKey add = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                add.SetValue("Reminder", "\"" + Application.ExecutablePath.ToString() + "\"");
+
+                Properties.Settings.Default.startedWithWindows = true;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void SettingForm_Load(object sender, EventArgs e)
+        {
+        }
+
+        #region events
+
+        private void comboBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as ComboBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            saveUserSetting();
+            start();
+            this.Hide();
+        }
+
+        private void OnTimedEvent(object sender, EventArgs e)
+        {
+            showAlert();
+        }
+
+        private void OnClickNotifyIcon(object sender, MouseEventArgs e)
+        {
+            showSettingForm();
+        }
+
+        private void settingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            showSettingForm();
+        }
+
+        private void SettingForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            exitProgram();
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            bool cursorNotInBar = Screen.GetWorkingArea(this).Contains(Cursor.Position);
+
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+            }
+        }
+
+        protected override void SetVisibleCore(bool value)
+        {
+            if (!allowVisible)
+            {
+                value = false;
+                if (!this.IsHandleCreated) CreateHandle();
+            }
+            base.SetVisibleCore(value);
+        }
+
+        #endregion
+
+        #region private functions
+
+        private void showSettingForm()
+        {
+            allowVisible = true;
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.Activate();
+        }
+        
+        private void start()
+        {
+            timer.Interval = int.Parse(timeValue.Text) * TIME_UNIT;
+            timer.Start();
+        }
+
+        private void showAlert()
+        {
+            MessageBox.Show(new Form { TopMost = true }, message.Text);
+        }
+
+        private void exitProgram()
+        {
+            this.Dispose();
+        }
+
+        private void saveUserSetting()
+        {
+            Properties.Settings.Default.timeValue = timeValue.Text;
+            Properties.Settings.Default.message = message.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        #endregion
+    }
+}
